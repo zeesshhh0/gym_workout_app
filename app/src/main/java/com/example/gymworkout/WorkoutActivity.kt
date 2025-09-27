e822f45131fd04565905c7ce173bc3c8559091c4
z3sh4n <ghaniwalamohmmad@gmail.com>
2025-09-27 11:56:58 +0530

    init

---
+++ app/src/main/java/com/example/gymworkout/WorkoutActivity.kt
@@ -0,0 +1,28 @@
+package com.example.gymworkout
+
+import android.os.Bundle
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.example.gymworkout.databinding.ActivityWorkoutBinding
+
+class WorkoutActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWorkoutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityWorkoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        binding.toolbarLayout.title = title
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .setAnchorView(R.id.fab).show()
+        }
+    }
+}
